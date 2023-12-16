90ea00a66059bc9f56186547c4b2edca454105d3
Mork <mfinerty1@hotmail.com>
2023-12-16 16:15:38 -0500

    add styles to TableCells in ResultsTable.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 // import { useState } from 'react'
-import SearchBar from './components/SearchBar';
-import ResultsTable from './components/ResultsTable';
+import SearchBar from "./components/SearchBar";
+import ResultsTable from "./components/ResultsTable";
 
 function App() {
-
   return (
-    <div>
+    <div style={{ height: "100vh", backgroundImage: "url('/background.jpg')" }}>
       <SearchBar />
       <ResultsTable />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ResultsTable.jsx
+++ src/components/ResultsTable.jsx
@@ -5,6 +5,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
 
 function createData(name, rank, lp, wins, losses) {
   const winRate = wins / (wins + losses);
@@ -21,57 +22,58 @@ const rows = [
 const headerCellStyle = {
   borderBottom: "2px solid #000",
   textDecoration: "underline",
+  fontWeight: "bold",
 };
 
 export default function ResultsTable() {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell sx={headerCellStyle}>
-              <strong>Summoner Name</strong>
-            </TableCell>
-            <TableCell align="right" sx={headerCellStyle}>
-              <strong>Rank</strong>
-            </TableCell>
-            <TableCell align="right" sx={headerCellStyle}>
-              <strong>LP</strong>
-            </TableCell>
-            <TableCell align="right" sx={headerCellStyle}>
-              <strong>Wins</strong>
-            </TableCell>
-            <TableCell align="right" sx={headerCellStyle}>
-              <strong>Losses</strong>
-            </TableCell>
-            <TableCell align="right" sx={headerCellStyle}>
-              <strong>Win Rate</strong>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
+    <Box sx={{ width: "90%", margin: "auto" }}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell sx={headerCellStyle}>Summoner Name</TableCell>
+              <TableCell align="right" sx={headerCellStyle}>
+                Rank
               </TableCell>
-              <TableCell align="right">{row.rank}</TableCell>
-              <TableCell align="right">{row.lp}</TableCell>
-              <TableCell align="right">{row.wins}</TableCell>
-              <TableCell align="right">{row.losses}</TableCell>
-              <TableCell align="right">
-                {row.winRate !== undefined
-                  ? `${(row.winRate * 100).toFixed(2)}%`
-                  : "-"}
+              <TableCell align="right" sx={headerCellStyle}>
+                LP
+              </TableCell>
+              <TableCell align="right" sx={headerCellStyle}>
+                Wins
+              </TableCell>
+              <TableCell align="right" sx={headerCellStyle}>
+                Losses
+              </TableCell>
+              <TableCell align="right" sx={headerCellStyle}>
+                Win Rate
               </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.rank}</TableCell>
+                <TableCell align="right">{row.lp}</TableCell>
+                <TableCell align="right">{row.wins}</TableCell>
+                <TableCell align="right">{row.losses}</TableCell>
+                <TableCell align="right">
+                  {row.winRate !== undefined
+                    ? `${(row.winRate * 100).toFixed(2)}%`
+                    : "-"}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 }
