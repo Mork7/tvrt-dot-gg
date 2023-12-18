02746800bd7b56a27859a85f291744e3f929a4e2
Mork7 <mfinerty1@hotmail.com>
2023-12-17 22:46:20 -0500

    add test div for left side profile

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import ResultsTable from "./components/ResultsTable";
 
 function App() {
   return (
-    <div style={{ backgroundColor: "#242b42", height: "100%"}}>
+    <div style={{ backgroundColor: "#242b42", height: "100%" }}>
       <SearchBar />
-      <ResultsTable />
+      <div style={{ display: "flex", flexDirection: "row" , alignItems: "space-between"}}>
+        <div style={{ height: "400px", width: "600px",  backgroundColor: "#212422" }} />
+        <ResultsTable />
+      </div>
     </div>
   );
 }
